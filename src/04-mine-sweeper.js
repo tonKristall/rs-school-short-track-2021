e3eb75feaabe542a5e0419eb719edaea97eb463b
tonKristall <krisale@yandex.ru>
2021-04-07 23:38:47 +0300

    feat: task 04 complete

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,48 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const minearr = [];
+  const str = matrix.length;
+  const stb = matrix[0].length;
+  for (let i = 0; i < str; i++) {
+    minearr[i] = [];
+    for (let j = 0; j < stb; j++) {
+      minearr[i][j] = null * 1;
+    }
+  }
+  for (let i = 0; i < str; i++) {
+    for (let j = 0; j < stb; j++) {
+      if (matrix[i][j] === true) {
+        if (i - 1 > -1) {
+          minearr[i - 1][j]++;
+          if (j + 1 < stb) {
+            minearr[i - 1][j + 1]++;
+          }
+          if (j - 1 > -1) {
+            minearr[i - 1][j - 1]++;
+          }
+        }
+        if (i + 1 < str) {
+          minearr[i + 1][j]++;
+          if (j + 1 < stb) {
+            minearr[i + 1][j + 1]++;
+          }
+          if (j - 1 > -1) {
+            minearr[i + 1][j - 1]++;
+          }
+        }
+        if (j + 1 < stb) {
+          minearr[i][j + 1]++;
+        }
+        if (j - 1 > -1) {
+          minearr[i][j - 1]++;
+        }
+      }
+      // minearr[i][j] = minearr[i][j] * 1;
+    }
+  }
+  return minearr;
 }
 
 module.exports = minesweeper;
