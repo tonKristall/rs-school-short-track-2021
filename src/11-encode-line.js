d8a9bff744a983c67ffdeaadfb80f7b2b6fc1587
tonKristall <krisale@yandex.ru>
2021-04-10 03:59:21 +0300

    feat: task 11 complete

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,27 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (str.length !== 0) {
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+      let n = 0;
+      const el = str[i];
+      if (el !== str[i + 1]) {
+        result += el;
+      } else {
+        while (el === str[i] && i < str.length) {
+          i++;
+          n++;
+        }
+        i--;
+        result += `${n}${el}`;
+        n = 0;
+      }
+    }
+    return result;
+  }
+  return str;
 }
 
 module.exports = encodeLine;
