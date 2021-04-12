061d7552e29b2b9be77e8d220e2724182d4105ce
tonKristall <krisale@yandex.ru>
2021-04-12 13:23:52 +0300

    feat: task 14 complete

--- src/14-queue.js
+++ src/14-queue.js
@@ -10,18 +10,53 @@
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  *
  */
+const ListNode = require('../extensions/list-node');
 
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
+  constructor() {
+    this.list = null;
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    return '';
+  }
+
+  enqueue(element) {
+    if (this.list === null) {
+      this.list = new ListNode(element);
+    } else {
+      const nextlist = new ListNode(element);
+      nextlist.next = this.list;
+      this.list = nextlist;
+    }
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    let nextlist = null;
+    let newlist = null;
+    while (this.list.next !== null) {
+      nextlist = new ListNode(this.list.value);
+      nextlist.next = newlist;
+      newlist = nextlist;
+      this.list = this.list.next;
+    }
+    const listup = this.list.value;
+    this.list = newlist;
+    if (this.list !== null) {
+      nextlist = null;
+      newlist = null;
+      while (this.list.next !== null) {
+        nextlist = new ListNode(this.list.value);
+        nextlist.next = newlist;
+        newlist = nextlist;
+        this.list = this.list.next;
+      }
+      nextlist = new ListNode(this.list.value);
+      nextlist.next = newlist;
+      newlist = nextlist;
+      this.list = newlist;
+    }
+    return listup;
   }
 }
 
